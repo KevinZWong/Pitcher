d5ab582f0860ae5ac4409fc7d8639e9c27fd0a74
kevinzwong <zwongkevin@gmail.com>
2025-02-15 19:19:54 -0800

    optimized

--- pitcher/convex/_generated/api.d.ts
+++ pitcher/convex/_generated/api.d.ts
@@ -16,6 +16,7 @@ import type {
 import type * as auth_ResendOTP from "../auth/ResendOTP.js";
 import type * as auth from "../auth.js";
 import type * as http from "../http.js";
+import type * as projects from "../projects.js";
 import type * as users from "../users.js";
 
 /**
@@ -30,6 +31,7 @@ declare const fullApi: ApiFromModules<{
   "auth/ResendOTP": typeof auth_ResendOTP;
   auth: typeof auth;
   http: typeof http;
+  projects: typeof projects;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
